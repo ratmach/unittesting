b739062979911b3fe54a15b1f404f21c3e46a7d0
Rati Matchavariani <rati.matchavariani@hellotend.com>
2022-10-20 14:51:01 +0400

    updated lifecycle tests to better match the theme

--- lsn/l3_lifecycles/tests/test_script.py
+++ lsn/l3_lifecycles/tests/test_script.py
@@ -1,6 +1,30 @@
-from lsn.l3_lifecycles.tests.utils import SetUpTest, SetUpClassTest, TearDownTest
+from lsn.l3_lifecycles.tests.utils import SetUpTest, SetUpClassTest, TearDownTest, TearDownClassTest, Jerry, Universes
 
 
-class TestStuff(SetUpTest):
-    def test_stuff(self):
-        pass
+# TODO : this is how to initiiiiiiiitialize jerry = Jerry(origin=Universes.some_universe)
+
+class TestJerryBoree(SetUpTest):
+    # TODO: tests are already set up,
+    #  i need a to waaa(:burp:)y to populate initial data
+    #  note to self: should proobably use setUp to do so
+    pass
+
+
+class TestJerryBoreeCloningMachine(SetUpClassTest):
+    # TODO: same jazz here,
+    #  in case i ever lose a jerry miiight be a good idea to clone him into each jerry-boree instance
+    #  in case i need to run several tests
+    #  note to self: should proobably use setUpClass to do so
+    pass
+
+
+class TestJerryBoreeReturnMechanism(TearDownTest):
+    # TODO: after tests are done should probably let them go wherever
+    #  note to self: should proobably use tearDown to do so
+    pass
+
+
+class TestJerryBoreeReturnMechanismCloningMachine(TearDownClassTest):
+    # TODO: almost forgot, should do the same for cloning machine tests
+    #  note to self: should proobably use tearDownClass to do so
+    pass
--- lsn/l3_lifecycles/tests/utils.py
+++ lsn/l3_lifecycles/tests/utils.py
@@ -1,29 +1,58 @@
+from dataclasses import dataclass
 from random import random
 from unittest import TestCase
 
+from lsn.l1_basic_mocking.conf import Universes
+
+
+@dataclass
+class Jerry:
+    origin: Universes
+    name: str = "jerry"
+
 
 class SetUpTest(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.value = random()
-        cls.expected_value = cls.value
+    def test_default(self):
+        pass
 
     def tearDown(self):
-        self.assertEqual(self.value, self.expected_value)
+        self.assertNotEqual(self.jerry, None)
+        self.assertEqual(self.jerry.name, "jerry")
+        self.assertEqual(self.jerry.origin, Universes.C137)
 
 
 class SetUpClassTest(TestCase):
-    def setUp(self):
-        self.value = random()
-        self.expected_value = self.value
+    def test_default(self):
+        pass
 
-    def tearDown(self):
-        self.assertEqual(self.value, self.expected_value)
+    @classmethod
+    def tearDownClass(cls):
+        assert (getattr(cls, "jerry", None) is not None)
+        assert (getattr(cls, "jerry", None).name == "jerry")
+        assert (getattr(cls, "jerry", None).origin == Universes.C137)
 
 
 class TearDownTest(TestCase):
-    def setUp(self) -> None:
-        self.some_stuff = 1234
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.jerry = Jerry(origin=Universes.C137)
 
-    def tearDown(self):
-        self.assertEqual(getattr(self, "some_stuff", None), None)
+    def test_default(self):
+        pass
+
+    def doCleanups(self) -> None:
+        self.assertEqual(getattr(self, "jerry", None), None)
+        super().doCleanups()
+
+
+class TearDownClassTest(TestCase):
+    def setUp(self) -> None:
+        self.jerry = Jerry(origin=Universes.C137)
+
+    def test_default(self):
+        pass
+
+    @classmethod
+    def doClassCleanups(cls) -> None:
+        assert (getattr(cls, "jerry", None) is None)
+        super().doClassCleanups()
