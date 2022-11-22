a96e923bad91a33777e567753df46073c8602aa9
AleksandarFa <aleksandar_fa@epam.com>
2022-11-22 20:09:13 +0100

    Test universe_hashing_algorithm.

--- lsn/l1_basic_mocking/tests/test_space_cruiser_debugger.py
+++ lsn/l1_basic_mocking/tests/test_space_cruiser_debugger.py
@@ -1,13 +1,15 @@
 from unittest import TestCase
 
-from lsn.l1_basic_mocking.app.script import debug_space_cruiser
+from lsn.l1_basic_mocking.app.script import debug_space_cruiser, get_universe_hash
 
 
 class TestSpaceCruiserDebugger(TestCase):
     # Exercise l1_A
     def test_universe_hashing_algorithm(self):
         # hint: check outputs for get_universe_hash algorithm
-        pass
+        self.assertEqual(get_universe_hash('C-132'), 67132)
+        self.assertEqual(get_universe_hash('D-132'), 68132)
+
 
     # Exercise l1_B
     def test_space_cruiser_debugging(self):
