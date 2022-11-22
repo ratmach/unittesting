89e850a0f29696ec03c46bd31d6b3bda28c3f63d
AleksandarFa <aleksandar_fa@epam.com>
2022-11-22 20:46:23 +0100

    Test SpaceCruiser.debug_space_cruiser.

--- lsn/l1_basic_mocking/tests/test_space_cruiser_debugger_oop.py
+++ lsn/l1_basic_mocking/tests/test_space_cruiser_debugger_oop.py
@@ -3,7 +3,7 @@ import logging
 from lsn.l1_basic_mocking.app.script_oop import SpaceCruiser
 from lsn.l1_basic_mocking.conf import UNIVERSE
 from lsn.l1_basic_mocking.tests.utils import OOPOnlyTestCase
-
+from unittest.mock import patch
 
 class TestOOPSpaceCruiserDebugger(OOPOnlyTestCase):
     # Exercise l1_A1
@@ -13,9 +13,14 @@ class TestOOPSpaceCruiserDebugger(OOPOnlyTestCase):
         self.assertEqual(SpaceCruiser.get_universe_hash('D-137'), 68137)
 
     # Exercise l1_B1
-    def test_space_cruiser_debugging(self):
+    @patch('lsn.l1_basic_mocking.app.script_oop.SpaceCruiser.check_quantum_carburetor')
+    @patch('lsn.l1_basic_mocking.app.script_oop.SpaceCruiser.get_universe_hash')
+    def test_space_cruiser_debugging(self, mock_universe_hash, mock_check_quantum_carburetor):
         cruiser = SpaceCruiser(universe=UNIVERSE, logger=logging.getLogger(__name__))
         # hint: review debug_space_cruiser and mock.patch specific functions
+        mock_universe_hash.return_value = 67137
+        mock_check_quantum_carburetor.return_value = False
         self.assertFalse(cruiser.debug_space_cruiser())
         # hint: review debug_space_cruiser and mock.patch specific functions
+        mock_check_quantum_carburetor.return_value = True
         self.assertTrue(cruiser.debug_space_cruiser())
