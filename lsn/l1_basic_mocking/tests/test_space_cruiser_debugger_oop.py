4a4992d9b947d174cdb16328297c1a29b2f36478
AleksandarFa <aleksandar_fa@epam.com>
2022-11-22 20:42:19 +0100

    Test SpaceCruiser.get_universe_hash.

--- lsn/l1_basic_mocking/tests/test_space_cruiser_debugger_oop.py
+++ lsn/l1_basic_mocking/tests/test_space_cruiser_debugger_oop.py
@@ -9,7 +9,8 @@ class TestOOPSpaceCruiserDebugger(OOPOnlyTestCase):
     # Exercise l1_A1
     def test_universe_hashing_algorithm(self):
         # hint: check outputs for get_universe_hash algorithm
-        pass
+        self.assertEqual(SpaceCruiser.get_universe_hash('C-137'), 67137)
+        self.assertEqual(SpaceCruiser.get_universe_hash('D-137'), 68137)
 
     # Exercise l1_B1
     def test_space_cruiser_debugging(self):
